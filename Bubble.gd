c3b55e970655a56d846ba4c611e6b58c80a5caf1
gsproston <gsproston94@gmail.com>
2024-05-08 23:04:28 +0100

    Rotating bubble

---
+++ Bubble.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+const RADIUS = 50
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _draw():
+	draw_arc(Vector2.ZERO, RADIUS, 0, TAU, 128, Color.BLACK, 1.0, true)
--- FishPath.gd
+++ FishPath.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 
+const BUBBLE_ANGLE_FACTOR = 1.0 / 10
+const RAIDUS = 250
+
+var bubble_angle = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,8 +14,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	bubble_angle += BUBBLE_ANGLE_FACTOR * delta
+	$Bubble.position = Vector2(RAIDUS, 0).rotated(bubble_angle)
 
 func _draw():
-	draw_arc(Vector2.ZERO, 250, 0, TAU, 128, Color.BLACK, 1.0, true)
+	draw_arc(Vector2.ZERO, RAIDUS, 0, TAU, 128, Color.BLACK, 1.0, true)
