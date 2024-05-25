595e97a0f78513172294c102dcd3f1be9edcd6b4
gsproston <gsproston94@gmail.com>
2024-05-25 20:40:57 +0100

    Won't swim past the target

--- Fish.gd
+++ Fish.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
+enum FISH_STATE {
+	IDLE, 
+	LOST
+}
+
 const LENGTH = 5
-const SPEED_DECREASE_RATE = 0.9
+const LOST_DISTANCE = 50
+const SWIM_SPEED_MIN = 10.0
+const SWIM_SPEED_MAX = 100.0
+const SWIM_SPEED_DECREASE_RATE = 0.9
 const SWIM_COOLDOWN_SECONDS = 2.0
 const TARGET_ROTATION_TOLERANCE = 0.01
-const TOP_SPEED = 100.0
 const TURN_SPEED = 5.0
 
-var speed = 0.0
+var speed = SWIM_SPEED_MIN
+var state = FISH_STATE.IDLE
 var swim_timer = SWIM_COOLDOWN_SECONDS
 var target_position = Vector2.ZERO
 var target_rotation = 0.0
@@ -20,20 +28,27 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# update the state
+	if (position.distance_to(target_position) > LOST_DISTANCE):
+		state = FISH_STATE.LOST
+	else:
+		state = FISH_STATE.IDLE
+	
 	# swim if we're facing the right direction
 	if (abs(rotation - target_rotation) < TARGET_ROTATION_TOLERANCE):
 		swim_timer += delta
 		position += Vector2.from_angle(rotation) * speed * delta
-		speed -= speed * SPEED_DECREASE_RATE * delta
+		var next_speed = speed - speed * SWIM_SPEED_DECREASE_RATE * delta
+		speed = max(SWIM_SPEED_MIN, next_speed);
 	else:
 		_rotate_to_target(delta)
 	
-	if (swim_timer > SWIM_COOLDOWN_SECONDS):
+	if (swim_timer > SWIM_COOLDOWN_SECONDS && state == FISH_STATE.LOST):
 		target_rotation = position.angle_to_point(target_position)
 		swim_timer = 0
 		# move towards the bubble
 		var direction = target_position - position
-		speed = TOP_SPEED
+		speed = SWIM_SPEED_MAX
 	
 	
 func _draw():
